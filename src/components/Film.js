8883683a0c2994e8096cd84a0b242d60c0e21185
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-10 22:09:57 +0000

    Added basic Film component

---
+++ src/components/Film.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Film = () => {
+  return (
+      <>
+      <p>Film</p>
+      </>
+  );
+};
+
+export default Film;
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Film from './Film';
 
 const FilmList = () => {
     return (
         <>
         <h3>Film List</h3>
+        <Film/>
+        <Film/>
+        <Film/>
         </>
     );
 };
