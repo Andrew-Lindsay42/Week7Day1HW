68bb3a5b8e556f0ffa6b1eef1dc3bddda32e4e24
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-10 22:08:16 +0000

    Added basic FilmList component

---
+++ src/components/FilmList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilmList = () => {
+    return (
+        <>
+        <h3>Film List</h3>
+        </>
+    );
+};
+
+export default FilmList;
--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import FilmList from '../components/FilmList';
 
 const FilmBox = () => {
     return (
         <>
         <h2>Upcoming Film Releases for UK</h2>
+        <FilmList/>
         </>
     );
 };
